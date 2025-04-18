625bca88bfaddb08300fd195b91d4048ac870f76
David Wolfenden <wolfdavo@gmail.com>
2025-04-18 01:01:28 -0700

    Added a bunch of plugins, and some nice structure. Just added Telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,88 +1,27 @@
-
+require 'core.options'
+require 'core.keymaps'
 -- ~/.config/nvim/init.lua
 -- Super minimal Lazy.nvim-based Neovim config
 
--- Set leader to space early
-vim.g.mapleader = " "
-
--- Core options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = true
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.lazyredraw = true
-vim.opt.ttyfast = true
-
--- Keymaps
-local keymap = vim.keymap.set
-keymap("n", "<Esc><Esc>", ":nohlsearch<CR>", { desc = "Clear search highlight" })
-keymap("n", "<leader>w", ":w<CR>", { desc = "Save" })
-keymap("n", "<leader>q", ":q<CR>", { desc = "Quit" })
-keymap("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic" })
-keymap("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-keymap("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
-keymap("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover Docs" })
-
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- Plugins
-require("lazy").setup({
-  {
-    "navarasu/onedark.nvim",
-    name = "onedark",
-    priority = 1000,
-    config = function()
-      require("onedark").setup {
-        style = "darker"
-      }
-      require("onedark").load()
-
-    end
-  },
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
   {
     "karb94/neoscroll.nvim",
     config = function()
       require("neoscroll").setup({
         easing_function = "quadratic"
-      })
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
       })
     end
   },
@@ -102,16 +41,12 @@ require("lazy").setup({
           },
         })
     end
-  }
+  },
+  require 'plugins.colortheme',
+  require 'plugins.neotree',
+  require 'plugins.bufferline',
+  require 'plugins.lualine',
+  require 'plugins.treesitter',
+  require 'plugins.telescope'
 })
 
--- Smooth faster scrolling with j/k
-local neoscroll = require("neoscroll")
-
-keymap("n", "<C-j>", function()
-  neoscroll.scroll(10, { move_cursor = true, duration = 100, easing = "quadratic" })  -- scroll 5 lines down over 100ms
-end, { desc = "Smooth scroll down" })
-
-keymap("n", "<C-k>", function()
-  neoscroll.scroll(-10, { move_cursor = true, duration = 100, easing = "quadratic" })  -- scroll 5 lines up over 100ms
-end, { desc = "Smooth scroll up" })
---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,27 @@
+-- Core options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.lazyredraw = true
+vim.opt.ttyfast = true
+vim.o.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.o.showtabline = 2 -- always show tabs
+vim.o.cmdheight = 1 -- more space in the neovim command line for displaying messages
+vim.opt.iskeyword:append '-' -- hyphenated words recognized by searches
+vim.o.splitbelow = true -- force all horizontal splits to go below current window
+vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
